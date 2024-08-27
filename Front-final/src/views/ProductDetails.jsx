fc8ed04061b6f19c9d96fb18526d8436bc401d26
DanteVasquezRivas <147457552+DanteVasquezRivas@users.noreply.github.com>
2024-08-27 00:42:03 -0400

    Modifying products

--- Front-final/src/views/ProductDetails.jsx
+++ Front-final/src/views/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../config/configAxios';
+import "./ProductsDetails.css"
 
 const ProductDetails = () => {
   const { id } = useParams();
